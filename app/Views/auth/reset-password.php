fbde290e9f30db3835bae13783e0b2d6fc1a030e
Mulyono <mulyonomuiz63@gmail.com>
2026-04-02 11:50:43 +0700

    membenahi tampilan input password

--- app/Views/auth/reset-password.php
+++ app/Views/auth/reset-password.php
@@ -11,9 +11,6 @@
             <input type="hidden" name="token" value="<?= $token; ?>">
             <div class="form">
                 <div id="password-field" class="field-wrapper input mb-2">
-                    <div class="d-flex justify-content-between">
-                        <label for="password">SANDI BARU</label>
-                    </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
                         <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                         <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
