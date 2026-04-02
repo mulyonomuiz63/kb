0e5bc2803b7ff16e861c9153cc5f5adb56246487
Mulyono <mulyonomuiz63@gmail.com>
2026-04-02 11:19:36 +0700

    update recapcha

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -111,7 +111,7 @@ class RegisterController extends BaseController
         // 3. Verifikasi reCAPTCHA
         if(setting('recaptcha_status') == 'true') {
             $token = $this->request->getPost('recaptcha_token');
-            if (!$this->verifyRecaptcha($token, 'registrasi')) {
+            if (!$this->verifyRecaptcha($token, 'akses-recaptcha')) {
                 session()->setFlashdata('pesan', "swal({title:'Info', text:'Verifikasi Bot Gagal', type:'info', padding:'2em'})");
                 return redirect()->to('auth/registrasi')->withInput();
             }
