442084f69463c8b2eb95959791b47fccc816e944
Mulyono <mulyonomuiz63@gmail.com>
2026-05-13 10:25:57 +0700

    nambah list ip

--- app/Filters/MaintenanceFilter.php
+++ app/Filters/MaintenanceFilter.php
@@ -21,7 +21,8 @@ class MaintenanceFilter implements FilterInterface
                 '::1',
                 '193.186.4.147',
                 '103.130.18.252',
-                '46.250.226.163' // <-- Masukkan IP dari gambar ke sini
+                '46.250.226.163',
+                '2407:3640:2227:600::1' // <-- Masukkan IP dari gambar ke sini
             ];
 
             // Dapatkan IP pengunjung saat ini
