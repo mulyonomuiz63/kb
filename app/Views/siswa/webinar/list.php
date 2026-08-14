88a9a6bab54e4fe6a9a3f638fdf7ebe400636352
Mulyono <mulyonomuiz63@gmail.com>
2026-08-14 09:39:29 +0700

    update webinar

--- app/Views/siswa/webinar/list.php
+++ app/Views/siswa/webinar/list.php
@@ -1047,7 +1047,7 @@
         const alertElement = document.getElementById('wa-group-alert');
 
         // Cek apakah user belum pernah menutup alert ini
-        if (!localStorage.getItem('wa_alert_hidden')) {
+        if (!localStorage.getItem('wa_alert_hidden_webinar')) {
             // Hapus d-none dan siapkan posisi di bawah layar (tersembunyi)
             alertElement.classList.remove('d-none');
             alertElement.style.transform = 'translate(-50%, 150%)';
@@ -1065,7 +1065,7 @@
             btn.addEventListener('click', function() {
 
                 // Simpan status ke localStorage browser
-                localStorage.setItem('wa_alert_hidden', 'true');
+                localStorage.setItem('wa_alert_hidden_webinar', 'true');
 
                 // Mainkan animasi turun ke bawah
                 alertElement.style.transform = 'translate(-50%, 150%)';
