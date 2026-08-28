d1ff5c616559955fff8bab1f688b6b452c93d29d
Mulyono <mulyonomuiz63@gmail.com>
2026-08-28 09:57:25 +0700

    whatsapp

--- app/Views/siswa/dashboard.php
+++ app/Views/siswa/dashboard.php
@@ -304,9 +304,9 @@
 
             <!-- Action Buttons -->
             <div class="d-flex flex-column flex-sm-row align-items-stretch align-items-sm-center mt-4 mt-md-0 gap-2 gap-sm-3 w-100 w-md-auto flex-shrink-0">
-                <button type="button" class="btn btn-sm btn-outline btn-outline-dashed btn-outline-success btn-active-light-success btn-wa-action fw-bold w-100 w-sm-auto text-nowrap" data-action="sudah">
+                <!-- <button type="button" class="btn btn-sm btn-outline btn-outline-dashed btn-outline-success btn-active-light-success btn-wa-action fw-bold w-100 w-sm-auto text-nowrap" data-action="sudah">
                     Sudah Bergabung
-                </button>
+                </button> -->
                 <a href="https://chat.whatsapp.com/GakLHM8gxxvJfHDEryZGOi?s=cl&p=a&mlu=4" target="_blank" class="btn btn-sm btn-success btn-wa-action fw-bold shadow-sm w-100 w-sm-auto text-nowrap" data-action="gabung">
                     Bergabung Sekarang
                 </a>
