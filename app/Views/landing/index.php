c040621fae18f07ecf28de4911a19931e6e3cc2c
Mulyono <mulyonomuiz63@gmail.com>
2026-09-01 14:06:42 +0700

    update tombol izin

--- app/Views/landing/index.php
+++ app/Views/landing/index.php
@@ -888,12 +888,12 @@
                                 <div class="mt-2 mb-2">
                                     <div class="row g-2 g-md-3">
                                         <div class="col-6">
-                                            <a href="<?= base_url('legalitas#lkp') ?>" class="badge text-bg-light p-3 p-md-3 rounded-pill text-primary btn-hover-dark text-nowrap w-100 text-center d-block" style="font-size: clamp(9px, 2.8vw, 12px);">
+                                            <a href="<?= base_url('legalitas#lkp') ?>" class="badge p-3 p-md-3 rounded-pill  btn-hover-dark text-nowrap w-100 text-center d-block" style="background-color: #fd7e14; font-size: clamp(9px, 2.8vw, 12px);">
                                                 LEGALITAS LEMBAGA
                                             </a>
                                         </div>
                                         <div class="col-6">
-                                            <a href="https://topcs.id/kelas-brevet" class="badge text-bg-success p-3 p-md-3 rounded-pill  btn-hover-dark text-nowrap w-100 text-center d-block" style="font-size: clamp(9px, 2.8vw, 12px);">
+                                            <a href="https://topcs.id/kelas-brevet" class="badge p-3 p-md-3 rounded-pill  btn-hover-dark text-nowrap w-100 text-center d-block" style="background-color: #25D366; font-size: clamp(9px, 2.8vw, 10px);">
                                                 KONSULTASI SEKARANG
                                             </a>
                                         </div>
