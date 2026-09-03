b96ddcd74a7ab9bceb8df30a366e4c52a5a80acf
Mulyono <mulyonomuiz63@gmail.com>
2026-09-03 10:13:07 +0700

    midtrans

--- app/Controllers/Siswa/TransaksiController.php
+++ app/Controllers/Siswa/TransaksiController.php
@@ -707,6 +707,7 @@ class TransaksiController extends BaseController
                         "country_code" => "IDN"
                     ),
                 ),
+                'enabled_payments' => ['credit_card', 'qris', 'va']
             );
 
             try {
