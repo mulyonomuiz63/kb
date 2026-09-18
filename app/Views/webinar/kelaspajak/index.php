188fb6f96799f174bfa2ab9c984693c7296154a9
Mulyono <mulyonomuiz63@gmail.com>
2026-09-18 15:54:59 +0700

    kelas pajak

--- app/Views/webinar/kelaspajak/index.php
+++ app/Views/webinar/kelaspajak/index.php
@@ -295,7 +295,13 @@ $paketUtama = !empty($daftarPaket) ? $daftarPaket[0] : null;
                     </div>
 
                     <div class="col-lg-6 text-center">
-                        <?php $videoUrl =  base_url('uploads/video/opening.mp4?v=' . time()); ?>
+                        <?php
+                        // Mendapatkan path fisik file di server untuk mengecek kapan terakhir diedit
+                        $videoPath = FCPATH . 'uploads/video/opening.mp4';
+                        $versi = file_exists($videoPath) ? filemtime($videoPath) : 1;
+
+                        $videoUrl = base_url('uploads/video/opening.mp4?v=' . $versi);
+                        ?>
                         <div class="ratio ratio-16x9 rounded-4 shadow-lg border border-3 border-white overflow-hidden" style="pointer-events: none;">
                             <video src="<?= $videoUrl ?>" autoplay muted loop playsinline style="object-fit: cover; width: 100%; height: 100%;"></video>
                         </div>
